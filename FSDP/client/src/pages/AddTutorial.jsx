867ff968c57ef85320596b484c2ccbd24d88ace6
Ryan <limkl.ryan@gmail.com>
2024-06-10 19:22:58 +0800

    new shit

---
+++ FSDP/client/src/pages/AddTutorial.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import http from '../http';
+
+function AddTutorial() {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+            title: "",
+            description: ""
+        },
+        validationSchema: yup.object({
+            title: yup.string().trim()
+                .min(3, 'Title must be at least 3 characters')
+                .max(100, 'Title must be at most 100 characters')
+                .required('Title is required'),
+            description: yup.string().trim()
+                .min(3, 'Description must be at least 3 characters')
+                .max(500, 'Description must be at most 500 characters')
+                .required('Description is required')
+        }),
+        onSubmit: (data) => {
+            data.title = data.title.trim();
+            data.description = data.description.trim();
+            http.post("/tutorial", data)
+                .then((res) => {
+                    console.log(res.data);
+                    navigate("/tutorials");
+                });
+        }
+    });
+
+    return (
+        <Box>
+            <Typography variant="h5" sx={{ my: 2 }}>
+                Add Tutorial
+            </Typography>
+            <Box component="form" onSubmit={formik.handleSubmit}>
+                <TextField
+                    fullWidth margin="dense" autoComplete="off"
+                    label="Title"
+                    name="title"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.title && Boolean(formik.errors.title)}
+                    helperText={formik.touched.title && formik.errors.title}
+                />
+                <TextField
+                    fullWidth margin="dense" autoComplete="off"
+                    multiline minRows={2}
+                    label="Description"
+                    name="description"
+                    value={formik.values.description}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.description && Boolean(formik.errors.description)}
+                    helperText={formik.touched.description && formik.errors.description}
+                />
+                <Box sx={{ mt: 2 }}>
+                    <Button variant="contained" type="submit">
+                        Add
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
+
+export default AddTutorial;
