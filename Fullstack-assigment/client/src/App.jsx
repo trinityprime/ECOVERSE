07c0fc88c3c68bc58e904c9c8bd2bb9880894a28
brian-sketch <stormenglish.sgp@gmail.com>
2024-08-10 13:34:43 +0800

    This updated App.jsx file now includes the import for the AboutUs component and adds a new route for the "/aboutus" path. With these changes, users will be able to navigate to the About Us page when they click the link in the footer (assuming the Footer component has been updated to use the Link component from react-router-dom for the About Us link). Make sure that your AboutUs.jsx file is located in the correct directory (./pages/AboutUs.jsx) and that it exports the AboutUs component correctly.

--- Fullstack-assigment/client/src/App.jsx
+++ Fullstack-assigment/client/src/App.jsx
@@ -32,6 +32,7 @@ import EditReport from './pages/EditReport';
 import AdminECManagement from './pages/AdminECManagement';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import AboutUs from './pages/aboutus'; 
 import Footer from './footer.jsx';
 import http from './http';
 import RequestOtp from './pages/RequestOtp';
@@ -299,6 +300,7 @@ function App() {
               <Route path="/AdminECManagement" element={<AdminECManagement />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/aboutus" element={<AboutUs />} />
               {/* <Route path="/request-otp" element={<RequestOtp />} />
               <Route path="/verify-otp" element={<VerifyOtp />} />
               <Route path="/reset-password" element={<ResetPassword />} /> */}
